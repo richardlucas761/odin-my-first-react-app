b338692fc01560eb7c8773a59fb936ddac43ad7d
Richard Lucas <richard@richardlucas.me.uk>
2026-06-18 14:31:08 +0100

    Update the list to accept a "Starts With" parameter.

--- odin-my-first-react-app/src/Animals.jsx
+++ odin-my-first-react-app/src/Animals.jsx
@@ -6,19 +6,20 @@ function List(props) {
     return (
         <ul>
             {props.animals.map((animal) => {
-                return <ListItem key={animal} animal={animal} />;
+                return animal.startsWith(props.startsWith) && <li key={animal}>{animal}</li>;
             })}
         </ul>
     );
 }
 
 function App() {
-    const animals = ["Lion", "Cow", "Snake", "Mongoose", "Haggis"];
+    const animals = ["Lion", "Cow", "Snake", "Mongoose", "Haggis", "Lemur", "Hippo"];
+    const startsWith = 'H';
 
     return (
         <div>
-            <h1>Animals</h1>
-            <List animals={animals} />
+            <h1>Animals whose name starts with '{startsWith}'</h1>
+            <List animals={animals} startsWith={startsWith} />
         </div>
     );
 }
