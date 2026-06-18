94f5c8b6c065c20f3e7b24a8db51252122aba3b1
Richard Lucas <richard@richardlucas.me.uk>
2026-06-18 14:20:40 +0100

    Create a list of items with map.

--- odin-my-first-react-app/src/main.jsx
+++ odin-my-first-react-app/src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 // import App from './App.jsx'
 // import Greeting from "./Greeting.jsx";
 // import DefaultFood, { SavouryFood } from "./Foods.jsx"
-import App from './Button.jsx';
+// import App from './Button.jsx';
+import App from './Animals.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
