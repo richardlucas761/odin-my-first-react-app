ab53cdea25628bcefdc39097637c9fec3a15704a
Richard Lucas <richard@richardlucas.me.uk>
2026-06-22 14:41:38 +0100

    Modify to enable changing both first and last name, using state.

--- odin-my-first-react-app/src/Person.jsx
+++ odin-my-first-react-app/src/Person.jsx
@@ -1,23 +1,28 @@
 import { useState } from 'react';
 
-// TODO: Add two separate input fields for the first name and the last name. 
-// Either of these should be able to update the full name in the h1 element with every keystroke.
+// See : https://react.dev/reference/react-dom/components/input#controlling-an-input-with-a-state-variable
 
 function Person() {
-    const [person, setPerson] = useState({ firstName: "John", lastName: "Doe", age: 33 });
-
-    const handleIncreaseAge = () => {
-        // copy the existing person object into a new object
-        // while updating the age property
-        const newPerson = { ...person, age: person.age + 1 };
-        setPerson(newPerson);
-    };
+    const [firstName, setFirstName] = useState('John');
+    const [lastName, setLastName] = useState('Doe');
 
     return (
         <>
-            <h1>{person.firstName} {person.lastName}</h1>
-            <h2>{person.age}</h2>
-            <button onClick={handleIncreaseAge}>Increase age by 1</button>
+            <h1>{firstName} {lastName}</h1>
+            <label>
+                First name:
+                <input
+                    value={firstName}
+                    onChange={e => setFirstName(e.target.value)}
+                />
+            </label>
+            <label>
+                Last name:
+                <input
+                    value={lastName}
+                    onChange={e => setLastName(e.target.value)}
+                />
+            </label>
         </>
     );
 }
